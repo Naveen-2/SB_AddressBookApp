ce25de7d706164caaeb34a79228f015fc05e6d8b
Naveen-2 <naveen2k15cse@gmail.com>
2022-05-27 10:46:38 +0530

    Lombok library added to DTO and Model

--- AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
@@ -2,7 +2,9 @@ package com.bridgelabz.addressbookapp.model;
 
 import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
 
-public class AddressBookData {
+import lombok.Data;
+
+public @Data class AddressBookData {
 	private int contactId;
     private String firstName;
     private String lastName;
@@ -28,75 +30,4 @@ public class AddressBookData {
         this.zipCode = addressBookDTO.zipCode;
     }
 
-    public int getContactId() {
-        return contactId;
-    }
-
-    public void setContactId(int contactId) {
-        this.contactId = contactId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
 }
